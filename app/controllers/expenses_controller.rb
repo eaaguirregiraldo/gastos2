2c8dcc63cb07f2cc701bcc60984a30fa1f969b1e
eaaguirregiraldo <eaaguirregiraldo@gmail.com>
2016-01-20 15:08:33 -0500

    Se adiciona busqueda por concepto

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,22 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:concept]
+      @expenses = Expense.where(concept: params[:concept]).order("date DESC")      
+    else
+      @expenses = Expense.order("date DESC")
+    end     
   end
 end
--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 module ExpensesHelper
 end
